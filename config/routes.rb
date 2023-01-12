d94a1cb175890306875ab27fe7ad5a5a11ad78a7
Axel Regairaz <axel.regairaz@qobuz.com>
2023-01-12 20:31:04 +0100

    Day 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "lists", to: "lists#index"
-  get "lists/new", to: "lists#new", as: :new
-  get "lists/:id", to: "lists#show", as: :show
-  post "lists", to: "lists#create"
+  # get "lists", to: "lists#index"
+  # get "lists/new", to: "lists#new", as: :new
+  # get "lists/:id", to: "lists#show", as: :show
+  # post "lists", to: "lists#create"
 
-  get "lists/:id/bookmarks/new", to: "bookmarks#new"
-  post "lists/:id/bookmarks", to: "bookmarks#create"
-  delete "bookmarks/:id", to: "bookmarks#delete"
+  # get "lists/:id/bookmarks/new", to: "bookmarks#new"
+  # post "lists/:id/bookmarks", to: "bookmarks#create"
+  # delete "bookmarks/:id", to: "bookmarks#delete"
+  root to: "lists#index"
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
+  end
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
